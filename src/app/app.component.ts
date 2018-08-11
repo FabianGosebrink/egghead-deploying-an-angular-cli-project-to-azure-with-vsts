9f244b4d50f11651b91d7ee824eca2ee43beaac2
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-08-11 15:52:14 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'deploying-an-angular-cli-project-to-azure-with-vsts';
+}
